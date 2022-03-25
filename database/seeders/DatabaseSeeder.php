56bd5f5bd050bd9d081e027954663883c4bc333b
rinov <rinov@armsolusi.com>
2022-03-25 21:46:09 +0700

    seed added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,5 +15,40 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $users = collect([
+            // [
+            //     'name' => "Administrator",
+            //     'email' => 'admin@gmail.com',
+            //     'nik' => 'administrator1',
+            //     'role' => 'Admin',
+            //     'password' => Hash::make('password'),
+            // ],
+            [
+                'name' => "User 1",
+                'email' => 'role@gmail.com',
+                'username' => 'lead',
+                // 'role' => 'lead',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => "User 2",
+                'email' => 'developer@gmail.com',
+                'username' => 'Developer',
+                // 'role' => 'Developer',
+                'password' => Hash::make('password'),
+            ],
+            [
+                'name' => "User 3",
+                'email' => 'leaddeveloper@gmail.com',
+                'username' => 'LeadDeveloper',
+                // 'role' => 'Lead Developer',
+                'password' => Hash::make('password'),
+            ],
+        ])->map(function ($users) {
+            User::create($users);
+        });
+
+
     }
 }
